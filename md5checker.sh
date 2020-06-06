55d415b4db649359fd20fe91407c395d60408cf5
root <root@DESKTOP-AAK4ICS.localdomain>
2020-06-05 22:37:50 -0300

    first commit

---
+++ md5checker.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+	echo "File argument cannot be 0"
+	echo "Exiting..."
+	exit 0
+fi
+
+file_hash=$(md5sum $1 | cut -d ' ' -f 1   )
+arg_hash=$2
+size_arg_hash=$(echo ${#arg_hash})
+size_md5_bytes=32
+echo
+echo "Verifying..."
+echo
+
+if [ $size_arg_hash != $size_md5_bytes ] # search for better validations
+then
+	echo "Invalid MD5 hash: exit 0"
+	exit 0
+fi
+
+if [ $file_hash == $arg_hash ] 
+then
+	echo
+	echo "Yup! $1 = $2"
+	echo "... done. "
+      	echo
+else 
+	echo "Fail! Hashes don't match. "
+	echo
+	echo "$file_hash is NOT equals to $arg_hash"
+	echo
+	echo "Bye!"
+fi
+	
