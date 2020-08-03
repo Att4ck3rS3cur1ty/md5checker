5a0f8172cedc64737e70a1e21b57049f83a7a537
kadson@elpescador.com.br <kadson@elpescador.com.br>
2020-08-03 17:49:28 -0300

    validating the number of parameters

--- md5checker.sh
+++ md5checker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[ "$#" -ne 2 ] && echo "Illegal parameters! USAGE: $0 <FILE> <HASH>" && exit
+
+
 if [ -z "$1" -o  -z "$2" ]
 then
 	echo "File argument cannot be < 2"
@@ -7,19 +10,27 @@ then
 	exit 0
 fi
 
-file_hash=$(md5sum $1 | cut -d ' ' -f 1   )
+md5sum $1 
+if [ $? -eq 0 ]; then
+	file_hash=$(md5sum $1 | cut -d ' ' -f 1   )	
+else 
+	exit 0
+fi 
+
 arg_hash=$2
 size_arg_hash=$(echo ${#arg_hash})
 size_md5_bytes=32
-echo
-echo "Verifying..."
-echo
 
 if [ $size_arg_hash != $size_md5_bytes ] # search for better validations
 then
-	echo "Invalid MD5 hash: exit 0"
+	echo
+	echo "The MD5 hash you've entered is invalid, exiting..."
 	exit 0
 fi
+
+echo
+echo "Verifying..."
+echo
 
 if [ $file_hash == $arg_hash ] 
 then
