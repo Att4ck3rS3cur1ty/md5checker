cbc7965d9420be3400844bba9c6f2f04f99cdbdf
att4ck3rs3cur1ty <none@none.com>
2020-08-05 16:22:25 -0300

    replacing fail or success messages

--- md5checker.sh
+++ md5checker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# messages to append in the beginning of the outputs
+success_msg="[SUCCESS]"
+fail_msg="[FAIL]"
+
 # checks if the number of arguments is valid
-[ "$#" -ne 2 ] && echo "Illegal number of arguments! USAGE: $0 <FILE> <HASH>" && exit 2
+[ "$#" -ne 2 ] && echo "$fail_msg Illegal number of arguments! USAGE: $0 <FILE> <HASH>" && exit 2
 
 file_or_directory="$1"
 
@@ -10,10 +14,10 @@ if [ -f "${file_or_directory}"  ]; then
 	echo "[SUCCESS] file found!" 
 
 elif [ -d "${file_or_directory}" ]; then
-	echo "${file_or_directory} is a directory. Exiting..."
+	echo "$fail_msg ${file_or_directory} is a directory. Exiting..."
 	exit 2
 else
-	echo "[FAIL] file not found! Exiting..." 
+	echo "$fail_msg file not found! Exiting..." 
 	exit 2
 fi
 
@@ -31,24 +35,24 @@ size_md5_bytes=32
 if [ $size_arg_hash != $size_md5_bytes ] # search for better validations
 then
 	echo
-	echo "The MD5 hash you've entered is invalid, exiting..."
+	echo "$fail_msg The MD5 hash you've entered is invalid, exiting..."
 	exit 0
 fi
 
-echo
+echo "**************"
 echo "Verifying..."
-echo
+echo "**************"
 
 if [ $file_hash == $arg_hash ] 
 then
 	echo
-	echo "Yup! $file_or_directory = $2"
+	echo "$success_msg Yup! $file_or_directory = $2"
 	echo "... done. "
-      	echo
+    echo
 else 
-	echo "Fail! Hashes don't match. "
+	echo "$fail_msg Hashes don't match. Exiting... "
 	echo
-	echo "$file_hash is NOT equals to $arg_hash"
+	echo "$file_hash is NOT equals to $arg_hash. Exiting..."
 	echo
 	echo "Bye!"
 fi
